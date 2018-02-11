a7971243d5a8d96dcba10fb85c949dc89c5d2dee
superdevelopertr <superdevelopertr@outlook.com>
2018-02-11 22:47:07 +0300

    some stream ex added

---
+++ hello-lambda/src/com/ailhanli/ex9/AdvanceStream.java
@@ -0,0 +1,21 @@
+package com.ailhanli.ex9;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class AdvanceStream {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.asList(1, 2, 3, 4, 5).stream().collect(Collectors.summingInt(a -> a)));
+
+		System.out.println(Arrays.asList(1, 2, 3, 4, 5).stream().collect(Collectors.averagingInt(x -> x)));
+		
+		System.out.println(Arrays.asList(1,2,3,4,5).stream().collect(Collectors.maxBy(Integer::compareTo)).get());
+		
+		Arrays.asList(1,2,3,4,5).stream().max(Integer::compareTo);
+		
+		System.out.println(IntStream.rangeClosed(1, 100).boxed().collect(Collectors.summingDouble(a -> a)));
+		
+	}
+}
